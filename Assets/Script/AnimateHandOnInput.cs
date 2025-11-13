d9be3d1bcbd6c34036675411415dc9d42947a5fe
Alex <innerchaosgames1@gmail.com>
2025-11-13 17:02:43 +0200

    movement done

---
+++ Assets/Script/AnimateHandOnInput.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHandOnInput : MonoBehaviour
+{
+    [SerializeField] private InputActionProperty pinchAnimationAction;
+    [SerializeField] private InputActionProperty gripAnimationAction;
+    [SerializeField] private Animator handAnimator;
+
+    void Update()
+    {
+        var triggerValue = pinchAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
+        var gripValue = gripAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", gripValue);
+    }
+}
